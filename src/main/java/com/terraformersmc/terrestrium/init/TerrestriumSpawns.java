dbac46a7da2e5a5c2127224a588a9c24e86d75cf
Brandon Davis <goldr1000ftw@gmail.com>
2019-08-04 16:24:26 -0500

    make it a creature and make it weigh less

--- src/main/java/com/terraformersmc/terrestrium/init/TerrestriumSpawns.java
+++ src/main/java/com/terraformersmc/terrestrium/init/TerrestriumSpawns.java
@@ -1,12 +1,11 @@
 package com.terraformersmc.terrestrium.init;
 
-import net.minecraft.entity.EntityCategory;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.Biomes;
 
 public class TerrestriumSpawns {
 	public static void sort() {
 		//weight min max
-		Biomes.DESERT.getEntitySpawnList(EntityCategory.AMBIENT).add(new Biome.SpawnEntry(TerrestriumEntities.ROADRUNNER, 5, 1, 3));
+		Biomes.DESERT.getEntitySpawnList(TerrestriumEntities.ROADRUNNER.getCategory()).add(new Biome.SpawnEntry(TerrestriumEntities.ROADRUNNER, 1, 1, 3));
 	}
 }
